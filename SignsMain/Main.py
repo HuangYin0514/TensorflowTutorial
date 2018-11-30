ab218ec2766b9b117dc750b467a3f1b716f71ecd
huangyin0611 <1071324110@qq.com>
2018-11-30 22:40:14 +0800

    model

--- SignsMain/Main.py
+++ SignsMain/Main.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
     X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
     index = 0
     plt.imshow(X_train_orig[index])
-    plt.show()
     print("y = {}".format(str(np.squeeze(Y_train_orig[:, index]))))
 
     # flatten the training and test images
@@ -24,8 +23,8 @@ if __name__ == '__main__':
     X_train = X_train_flatten / 255
     X_test = X_test_flatten / 255
     # Convert training and test labels to one hot matrices
-    Y_train = convert_to_one_hot(Y_train_orig, 6)
-    Y_test = convert_to_one_hot(Y_test_orig, 6)
+    Y_train = one_hot_matrix(Y_train_orig.reshape(-1), 6)
+    Y_test = one_hot_matrix(Y_test_orig.reshape(-1), 6)
 
     print("number of training examples = " + str(X_train.shape[1]))
     print("number of test examples = " + str(X_test.shape[1]))
@@ -33,3 +32,33 @@ if __name__ == '__main__':
     print("Y_train shape: " + str(Y_train.shape))
     print("X_test shape: " + str(X_test.shape))
     print("Y_test shape: " + str(Y_test.shape))
+
+    X, Y = create_placeholders(12288, 6)
+    print("X = {}".format(str(X)))
+    print("Y = {}".format(str(Y)))
+
+    tf.reset_default_graph()
+    with tf.Session() as session:
+        parameters = initialize_parameters()
+        print("W1 = " + str(parameters["W1"]))
+        print("b1 = " + str(parameters["b1"]))
+        print("W2 = " + str(parameters["W2"]))
+        print("b2 = " + str(parameters["b2"]))
+
+    tf.reset_default_graph()
+    with tf.Session() as session:
+        X, Y = create_placeholders(12288, 6)
+        parameters = initialize_parameters()
+        Z3 = forward_propagation(X, parameters)
+        print("Z3 = {}".format(str(Z3)))
+
+    tf.reset_default_graph()
+    with tf.Session() as session:
+        X, Y = create_placeholders(12288, 6)
+        parameters = initialize_parameters()
+        Z3 = forward_propagation(X, parameters)
+        cost = compute_cost(Z3, Y)
+        print("cost = {}".format(str(cost)))
+
+    parameters = model(X_train, Y_train, X_test, Y_test,num_epochs=100)
+
